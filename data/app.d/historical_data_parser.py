2a181137a921892779874101dbf114bf6aa61aed
Jake Mulford <jakemulf@gmail.com>
2022-07-11 16:29:13 -0400

    almost there

--- data/app.d/historical_data_parser.py
+++ data/app.d/historical_data_parser.py
@@ -3,70 +3,75 @@ import deephaven.dtypes as dht
 
 import json
 
-def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None):
+def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None, table_to_write=None):
     """
     Extracts data from a SportRadar API JSON response into Deephaven tables
 
     Parameters:
-        d (dict): The SportRadar API JSON response as a Python dictionary
+        d (dict|list): The SportRadar API JSON response as a Python dictionary or list
         response_dictionary (dict): The response_dictionary used to store info for recursive calls
         parent_keys (list<str>): The JSON keys that led to the current dictionary in recursive calls
+        table_to_write (str): The key in the response_dictionary to write to
     Returns:
         dict<str, DynamicTableWriter>: Key value pairs that map to Deephaven table writers
     """
     if response_dictionary is None:
-        dtw_columns = {
-            "Key": dht.string,
-            "Value": dht.string
-        }
-        dtw = DynamicTableWriter(dtw_columns)
-
         response_dictionary = {}
-        response_dictionary["meta"] = dtw
-
+    if table_to_write is None:
+        table_to_write = "meta"
     if parent_keys is None:
-        parent_keys = []
+        parent_keys = ["meta"]
 
-    for key in d.keys():
+    iter_ = None
+    if isinstance(d, dict):
+        iter_ = d.keys()
+    elif isinstance(d, list):
+        iter_ = range(len(d))
+
+    write_values = {}
+    for key in iter_:
         #If dictionary, simply recurse with the new key appended to parent_keys
         if isinstance(d[key], dict):
-            extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key])
-
-        #If list, create a new table
+            new_parent_keys = parent_keys[0:-1] + [parent_keys[-1] + f"_{key}"]
+            extract_sport_radar_json(d[key], response_dictionary, new_parent_keys, table_to_write)
+        #If list, recurse with with the new key appended to parent_keys and a new table to write
         elif isinstance(d[key], list):
-            is_primitives = True
-            dtw_columns = {}
-            #If the items in the list are dictionaries, add columns to write
-            #Since the API doesn't include nulls, we have to loop through all of the items
-            #to get the proper schema
-            if isinstance(d[key][0], dict):
-                is_primitives = False
-                for i in range(len(d[key])):
-                    for list_key in d[key][i].keys():
-                        dtw_columns[list_key] = dht.string #potential TODO: Write other types?
-            else:
-                dtw_columns = {
-                    "Value": dht.string
-                }
-            dtw = DynamicTableWriter(dtw_columns)
-
-            rd_key = "_".join(parent_keys + [key])
-            response_dictionary[rd_key] = dtw
-            for list_item in d[key]:
-                if is_primitives:
-                    row_to_write = [json.dumps(list_item)]
-                else:
-                    row_to_write = []
-                    for list_key in dtw_columns.keys(): #Python 3.7: Dicts are ordered, so this will match the DTW columns
-                        if list_key in list_item: #Sanity check in case schemas differ
-                            row_to_write.append(str(list_item[list_key]))
-                        else:
-                            row_to_write.append("")
-                dtw.write_row(row_to_write)
-
-        #If primitive, append it to the meta table
+            extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key], table_to_write + f"_{str(key)}")
+        #If primitive, write it
         else:
-            meta_key = "_".join(parent_keys + [key])
-            response_dictionary["meta"].write_row((meta_key, json.dumps(d[key])))
+            write_values[key] = str(d[key])
+
+    if len(write_values) > 0:
+        if not table_to_write in response_dictionary.keys():
+            dtw_columns = {}
+            last_write = None
+            for parent_key in parent_keys:
+                if isinstance(parent_key, str):
+                    dtw_columns[parent_key] = dht.string
+                    last_write = parent_key
+                #We shouldn't get an int as the first item, could be fixed though
+                elif isinstance(parent_key, int):
+                    dtw_columns[last_write] = dht.int_
+            for write_key in write_values.keys():
+                dtw_columns[write_key] = dht.string
+
+            dtw = DynamicTableWriter(dtw_columns)
+            response_dictionary[table_to_write] = dtw
+
+        parent_keys_to_write = []
+        for parent_key in parent_keys:
+            #If an int is found, the previous value is its identifier, we don't
+            #need to write the identifier of an int (since it's in the column name),
+            #just the index
+            if isinstance(parent_key, int):
+                parent_keys_to_write[-1] = parent_key
+            else:
+                parent_keys_to_write.append(parent_key)
+
+        row_to_write = parent_keys_to_write + [write_values[write_key] for write_key in write_values.keys()]
+        try:
+            response_dictionary[table_to_write].write_row(row_to_write)
+        except:
+            pass #BAD, REMOVE, TODO
 
     return response_dictionary

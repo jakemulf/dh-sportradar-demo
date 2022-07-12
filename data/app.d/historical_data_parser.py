fd6d5b6baa2f77f5679a64b38095507ae7969466
Jake Mulford <jakemulf@gmail.com>
2022-07-12 11:16:55 -0400

    much improvement

--- data/app.d/historical_data_parser.py
+++ data/app.d/historical_data_parser.py
@@ -3,6 +3,94 @@ import deephaven.dtypes as dht
 
 import json
 
+class MissingNullsJsonWriter:
+    """
+    A class that writes rows to a Deephaven table that handles missing null values in a JSON object.
+
+    Attributes:
+        rows (list<dict>): A list of dictionaries representing each row to write
+        columns (dict): A dictionary representing the columns to write
+    """
+
+    def __init__(self):
+        self.rows = []
+        self.columns = {}
+
+    def add_row(self, row_to_add):
+        """
+        Adds the row to the list of rows to write
+        
+        Parameters:
+            row_to_add (dict): A dictionary representing a single row to write
+        Returns:
+            None
+        """
+        #Check the schema since SportRadar doesn't include nulls
+        for column in row_to_add:
+            if not (column in self.columns):
+                self.columns[column] = dht.string #TODO: Write more than just strings
+
+        self.rows.append(row_to_add)
+
+    def create_table(self):
+        """
+        Creates the Deephaven table for the given data
+
+        Returns:
+            Table: The Deephaven table with the data written to it, handling nulls as empty strings
+        """
+        dtw = DynamicTableWriter(self.columns)
+
+        for row in self.rows:
+            row_to_write = []
+            for column in self.columns:
+                #Another null check
+                if column in row:
+                    row_to_write.append(str(row[column]))
+                else:
+                    row_to_write.append("")
+            dtw.write_row(row_to_write)
+
+        return dtw.table
+
+def flatten_dict(d, key_prefix=None):
+    """
+    Flattens the given dictionary by changing nested dictionaries into upper level primitives.
+
+    For example:
+        {
+            "a": {
+                "b": "c",
+                "d": "e"
+            }
+        }
+
+    becomes
+        {
+            "a_b": "c",
+            "a_d": "e",
+        }
+
+    Parameters:
+        d (dict): The dictionary to flatten
+        key_prefix (str): The prefix for flattened keys on recursive calls
+    Returns:
+        dict: The flattened dictionary
+    """
+    flattened_d = {}
+    for key in d:
+        if key_prefix is None:
+            next_key_prefix = key
+        else:
+            next_key_prefix = f"{key_prefix}_{key}"
+
+        if isinstance(d[key], dict):
+            flattened_d = {**flattened_d, **flatten_dict(d[key], next_key_prefix)}
+        else:
+            flattened_d[next_key_prefix] = d[key]
+
+    return flattened_d
+
 def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None, table_to_write=None):
     """
     Extracts data from a SportRadar API JSON response into Deephaven tables
@@ -18,22 +106,24 @@ def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None, tabl
     if response_dictionary is None:
         response_dictionary = {}
     if table_to_write is None:
-        table_to_write = "meta"
+        table_to_write = "base"
     if parent_keys is None:
-        parent_keys = ["meta"]
+        parent_keys = ["base"]
 
     iter_ = None
+    is_list = False
     if isinstance(d, dict):
+        d = flatten_dict(d)
         iter_ = d.keys()
     elif isinstance(d, list):
         iter_ = range(len(d))
+        is_list = True
 
     write_values = {}
     for key in iter_:
         #If dictionary, simply recurse with the new key appended to parent_keys
         if isinstance(d[key], dict):
-            new_parent_keys = parent_keys[0:-1] + [parent_keys[-1] + f"_{key}"]
-            extract_sport_radar_json(d[key], response_dictionary, new_parent_keys, table_to_write)
+            extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key], table_to_write)
         #If list, recurse with with the new key appended to parent_keys and a new table to write
         elif isinstance(d[key], list):
             extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key], table_to_write + f"_{str(key)}")
@@ -41,37 +131,10 @@ def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None, tabl
         else:
             write_values[key] = str(d[key])
 
+    if not table_to_write in response_dictionary:
+        response_dictionary[table_to_write] = MissingNullsJsonWriter()
+
     if len(write_values) > 0:
-        if not table_to_write in response_dictionary.keys():
-            dtw_columns = {}
-            last_write = None
-            for parent_key in parent_keys:
-                if isinstance(parent_key, str):
-                    dtw_columns[parent_key] = dht.string
-                    last_write = parent_key
-                #We shouldn't get an int as the first item, could be fixed though
-                elif isinstance(parent_key, int):
-                    dtw_columns[last_write] = dht.int_
-            for write_key in write_values.keys():
-                dtw_columns[write_key] = dht.string
-
-            dtw = DynamicTableWriter(dtw_columns)
-            response_dictionary[table_to_write] = dtw
-
-        parent_keys_to_write = []
-        for parent_key in parent_keys:
-            #If an int is found, the previous value is its identifier, we don't
-            #need to write the identifier of an int (since it's in the column name),
-            #just the index
-            if isinstance(parent_key, int):
-                parent_keys_to_write[-1] = parent_key
-            else:
-                parent_keys_to_write.append(parent_key)
-
-        row_to_write = parent_keys_to_write + [write_values[write_key] for write_key in write_values.keys()]
-        try:
-            response_dictionary[table_to_write].write_row(row_to_write)
-        except:
-            pass #BAD, REMOVE, TODO
+        response_dictionary[table_to_write].add_row(write_values)
 
     return response_dictionary
--- data/notebooks/sample_file_readers.py
+++ data/notebooks/sample_file_readers.py
@@ -3,13 +3,20 @@ with open("/data/sample_json/daily_change_log.json", "r") as f:
     d = json.loads(f.read())
 table_writers = extract_sport_radar_json(d)
 for key in table_writers.keys():
-    globals()[f"daily_change_log_{key}"] = table_writers[key].table
+    globals()[f"daily_change_log_{key}"] = table_writers[key].create_table()
 
 with open("/data/sample_json/transactions.json", "r") as f:
     d = json.loads(f.read())
 table_writers = extract_sport_radar_json(d)
 for key in table_writers.keys():
-    globals()[f"transactions_{key}"] = table_writers[key].table
+    globals()[f"transactions_{key}"] = table_writers[key].create_table()
+
+d = None
+with open("/data/sample_json/play_by_play.json", "r") as f:
+    d = json.loads(f.read())
+table_writers = extract_sport_radar_json(d)
+for key in table_writers.keys():
+    globals()[f"play_by_play_{key}"] = table_writers[key].create_table()
 
 #NOTE: This section takes a while to run, which is why it doesn't run by default. You can
 #change run_long_section to True to run it
@@ -19,4 +26,4 @@ if run_long_section:
         d = json.loads(f.read())
     table_writers = extract_sport_radar_json(d)
     for key in table_writers.keys():
-        globals()[f"league_leaders_{key}"] = table_writers[key].table
+        globals()[f"league_leaders_{key}"] = table_writers[key].create_table()

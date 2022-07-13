3acc4745d6bcca70b2d7e4098eaa4e825041e923
Jake Mulford <jakemulf@gmail.com>
2022-07-13 14:18:02 -0400

    Nested json list improvement (#4)
    * almost there
    
    * much improvement
    
    * fixed API request sample

--- data/app.d/historical_data_parser.py
+++ data/app.d/historical_data_parser.py
@@ -3,70 +3,138 @@ import deephaven.dtypes as dht
 
 import json
 
-def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None):
+class MissingNullsJsonWriter:
+    """
+    A class that writes rows to a Deephaven table that handles missing null values in a JSON object.
+
+    Attributes:
+        rows (list<dict>): A list of dictionaries representing each row to write
+        columns (dict): A dictionary representing the columns to write
+    """
+
+    def __init__(self):
+        self.rows = []
+        self.columns = {}
+
+    def add_row(self, row_to_add):
+        """
+        Adds the row to the list of rows to write
+        
+        Parameters:
+            row_to_add (dict): A dictionary representing a single row to write
+        Returns:
+            None
+        """
+        #Check the schema since SportRadar doesn't include nulls
+        for column in row_to_add:
+            if not (column in self.columns):
+                self.columns[column] = dht.string #TODO: Write more than just strings
+
+        self.rows.append(row_to_add)
+
+    def create_table(self):
+        """
+        Creates the Deephaven table for the given data
+
+        Returns:
+            Table: The Deephaven table with the data written to it, handling nulls as empty strings
+        """
+        dtw = DynamicTableWriter(self.columns)
+
+        for row in self.rows:
+            row_to_write = []
+            for column in self.columns:
+                #Another null check
+                if column in row:
+                    row_to_write.append(str(row[column]))
+                else:
+                    row_to_write.append("")
+            dtw.write_row(row_to_write)
+
+        return dtw.table
+
+def flatten_dict(d, key_prefix=None):
+    """
+    Flattens the given dictionary by changing nested dictionaries into upper level primitives.
+
+    For example:
+        {
+            "a": {
+                "b": "c",
+                "d": "e"
+            }
+        }
+
+    becomes
+        {
+            "a_b": "c",
+            "a_d": "e",
+        }
+
+    Parameters:
+        d (dict): The dictionary to flatten
+        key_prefix (str): The prefix for flattened keys on recursive calls
+    Returns:
+        dict: The flattened dictionary
+    """
+    flattened_d = {}
+    for key in d:
+        if key_prefix is None:
+            next_key_prefix = key
+        else:
+            next_key_prefix = f"{key_prefix}_{key}"
+
+        if isinstance(d[key], dict):
+            flattened_d = {**flattened_d, **flatten_dict(d[key], next_key_prefix)}
+        else:
+            flattened_d[next_key_prefix] = d[key]
+
+    return flattened_d
+
+def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None, table_to_write=None):
     """
     Extracts data from a SportRadar API JSON response into Deephaven tables
 
     Parameters:
-        d (dict): The SportRadar API JSON response as a Python dictionary
+        d (dict|list): The SportRadar API JSON response as a Python dictionary or list
         response_dictionary (dict): The response_dictionary used to store info for recursive calls
         parent_keys (list<str>): The JSON keys that led to the current dictionary in recursive calls
+        table_to_write (str): The key in the response_dictionary to write to
     Returns:
         dict<str, DynamicTableWriter>: Key value pairs that map to Deephaven table writers
     """
     if response_dictionary is None:
-        dtw_columns = {
-            "Key": dht.string,
-            "Value": dht.string
-        }
-        dtw = DynamicTableWriter(dtw_columns)
-
         response_dictionary = {}
-        response_dictionary["meta"] = dtw
-
+    if table_to_write is None:
+        table_to_write = "base"
     if parent_keys is None:
-        parent_keys = []
+        parent_keys = ["base"]
 
-    for key in d.keys():
+    iter_ = None
+    is_list = False
+    if isinstance(d, dict):
+        d = flatten_dict(d)
+        iter_ = d.keys()
+    elif isinstance(d, list):
+        iter_ = range(len(d))
+        is_list = True
+
+    write_values = {}
+    for key in iter_:
         #If dictionary, simply recurse with the new key appended to parent_keys
         if isinstance(d[key], dict):
-            extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key])
-
-        #If list, create a new table
+            extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key], table_to_write)
+        #If list, recurse with with the new key appended to parent_keys and a new table to write
         elif isinstance(d[key], list):
-            is_primitives = True
-            dtw_columns = {}
-            #If the items in the list are dictionaries, add columns to write
-            #Since the API doesn't include nulls, we have to loop through all of the items
-            #to get the proper schema
-            if isinstance(d[key][0], dict):
-                is_primitives = False
-                for i in range(len(d[key])):
-                    for list_key in d[key][i].keys():
-                        dtw_columns[list_key] = dht.string #potential TODO: Write other types?
-            else:
-                dtw_columns = {
-                    "Value": dht.string
-                }
-            dtw = DynamicTableWriter(dtw_columns)
-
-            rd_key = "_".join(parent_keys + [key])
-            response_dictionary[rd_key] = dtw
-            for list_item in d[key]:
-                if is_primitives:
-                    row_to_write = [json.dumps(list_item)]
-                else:
-                    row_to_write = []
-                    for list_key in dtw_columns.keys(): #Python 3.7: Dicts are ordered, so this will match the DTW columns
-                        if list_key in list_item: #Sanity check in case schemas differ
-                            row_to_write.append(str(list_item[list_key]))
-                        else:
-                            row_to_write.append("")
-                dtw.write_row(row_to_write)
-
-        #If primitive, append it to the meta table
+            extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key], table_to_write + f"_{str(key)}")
+        #If primitive, write it
         else:
-            meta_key = "_".join(parent_keys + [key])
-            response_dictionary["meta"].write_row((meta_key, json.dumps(d[key])))
+            write_values[key] = str(d[key])
+
+    if not table_to_write in response_dictionary:
+        response_dictionary[table_to_write] = MissingNullsJsonWriter()
+
+    if len(write_values) > 0:
+        response_dictionary[table_to_write].add_row(write_values)
 
     return response_dictionary
--- data/notebooks/sample_api_request_reader.py
+++ data/notebooks/sample_api_request_reader.py
@@ -11,4 +11,4 @@ except KeyError:
 api_response_json = requests.get(f"http://api.sportradar.us/mlb/trial/v7/en/games/2021/REG/schedule.json?api_key={SPORT_RADAR_API_KEY}").json()
 table_writers = extract_sport_radar_json(api_response_json)
 for key in table_writers.keys():
-    globals()[f"league_schedule_{key}"] = table_writers[key].table
+    globals()[f"league_schedule_{key}"] = table_writers[key].create_table()

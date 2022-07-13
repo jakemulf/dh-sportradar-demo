d3877d0da6aece88651181d8a02fbf6e8ae5375b
Jake Mulford <jakemulf@gmail.com>
2022-07-13 15:44:54 -0400

    added type inference

--- data/app.d/historical_data_parser.py
+++ data/app.d/historical_data_parser.py
@@ -10,11 +10,45 @@ class MissingNullsJsonWriter:
     Attributes:
         rows (list<dict>): A list of dictionaries representing each row to write
         columns (dict): A dictionary representing the columns to write
+        datetime_converter (method): A method that takes a string and converts it
+            to a Deephaven DateTime object. If the string isn't convertable to a
+            DateTime object, None should be returned
     """
 
-    def __init__(self):
+    def __init__(self, datetime_converter=None):
         self.rows = []
         self.columns = {}
+        self.datetime_converter = datetime_converter
+
+    def _build_columns(self):
+        """
+        Builds the columns attribute based on the current rows
+
+        Returns:
+            None
+        """
+        self.columns = {}
+
+        for row in self.rows:
+            for column in row:
+                if not (column in self.columns):
+                    type_ = None
+                    #NOTE: The order of these if statements DOES matter. The bool check
+                    #needs to come before the int check. Why? Because bool is a subclass of int
+                    #https://stackoverflow.com/questions/37888620/comparing-boolean-and-int-using-isinstance
+                    if isinstance(row[column], bool):
+                        type_ = dht.bool_
+                    elif isinstance(row[column], int):
+                        type_ = dht.int_
+                    elif isinstance(row[column], float):
+                        type_ = dht.double
+                    #Check if it could be a DateTime
+                    elif (self.datetime_converter is not None) and (self.datetime_converter(row[column]) is not None):
+                        type_ = dht.DateTime
+                    #Default to string
+                    else:
+                        type_ = dht.string
+                    self.columns[column] = type_
 
     def add_row(self, row_to_add):
         """
@@ -25,11 +59,6 @@ class MissingNullsJsonWriter:
         Returns:
             None
         """
-        #Check the schema since SportRadar doesn't include nulls
-        for column in row_to_add:
-            if not (column in self.columns):
-                self.columns[column] = dht.string #TODO: Write more than just strings
-
         self.rows.append(row_to_add)
 
     def create_table(self):
@@ -39,6 +68,8 @@ class MissingNullsJsonWriter:
         Returns:
             Table: The Deephaven table with the data written to it, handling nulls as empty strings
         """
+        self._build_columns()
+
         dtw = DynamicTableWriter(self.columns)
 
         for row in self.rows:
@@ -46,9 +77,13 @@ class MissingNullsJsonWriter:
             for column in self.columns:
                 #Another null check
                 if column in row:
-                    row_to_write.append(str(row[column]))
+                    #Check if DateTime, if so convert to a Deephaven DateTime object
+                    if self.columns[column] == dht.DateTime:
+                        row_to_write.append(self.datetime_converter(row[column]))
+                    else:
+                        row_to_write.append(row[column])
                 else:
-                    row_to_write.append("")
+                    row_to_write.append(None)
             dtw.write_row(row_to_write)
 
         return dtw.table
@@ -91,7 +126,7 @@ def flatten_dict(d, key_prefix=None):
 
     return flattened_d
 
-def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None, table_to_write=None):
+def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None, table_to_write=None, datetime_converter=None):
     """
     Extracts data from a SportRadar API JSON response into Deephaven tables
 
@@ -100,6 +135,9 @@ def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None, tabl
         response_dictionary (dict): The response_dictionary used to store info for recursive calls
         parent_keys (list<str>): The JSON keys that led to the current dictionary in recursive calls
         table_to_write (str): The key in the response_dictionary to write to
+        datetime_converter (method): A method that takes a string and converts it
+            to a Deephaven DateTime object. If the string isn't convertable to a
+            DateTime object, None should be returned
     Returns:
         dict<str, DynamicTableWriter>: Key value pairs that map to Deephaven table writers
     """
@@ -123,16 +161,16 @@ def extract_sport_radar_json(d, response_dictionary=None, parent_keys=None, tabl
     for key in iter_:
         #If dictionary, simply recurse with the new key appended to parent_keys
         if isinstance(d[key], dict):
-            extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key], table_to_write)
+            extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key], table_to_write, datetime_converter)
         #If list, recurse with with the new key appended to parent_keys and a new table to write
         elif isinstance(d[key], list):
-            extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key], table_to_write + f"_{str(key)}")
+            extract_sport_radar_json(d[key], response_dictionary, parent_keys + [key], table_to_write + f"_{str(key)}", datetime_converter)
         #If primitive, write it
         else:
-            write_values[key] = str(d[key])
+            write_values[key] = d[key]
 
     if not table_to_write in response_dictionary:
-        response_dictionary[table_to_write] = MissingNullsJsonWriter()
+        response_dictionary[table_to_write] = MissingNullsJsonWriter(datetime_converter=datetime_converter)
 
     if len(write_values) > 0:
         response_dictionary[table_to_write].add_row(write_values)
--- data/notebooks/sample_file_readers.py
+++ data/notebooks/sample_file_readers.py
@@ -1,3 +1,21 @@
+from deephaven.time import to_datetime
+from dateutil import parser
+
+import re
+
+def datetime_converter(datetime_str):
+    match = re.match(r'(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})', datetime_str)
+    
+    if not bool(match):
+        return None
+
+    try:
+        dt = parser.parse(datetime_str)
+        dts = dt.strftime("%Y-%m-%dT%H:%M:%S") + " UTC"
+        return to_datetime(dts)
+    except:
+        return None
+
 d = None
 with open("/data/sample_json/daily_change_log.json", "r") as f:
     d = json.loads(f.read())
@@ -14,7 +32,7 @@ for key in table_writers.keys():
 d = None
 with open("/data/sample_json/play_by_play.json", "r") as f:
     d = json.loads(f.read())
-table_writers = extract_sport_radar_json(d)
+table_writers = extract_sport_radar_json(d, datetime_converter=datetime_converter)
 for key in table_writers.keys():
     globals()[f"play_by_play_{key}"] = table_writers[key].create_table()
 
